f2d9e03edc69fdd01fa63013870d5a55635c0389
fbronchaly <fbronchaly@gmail.com>
2020-04-11 18:27:30 +0200

    1 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavBarComponent } from './component/shared/nav-bar/nav-bar.component';
 import { PrincipalComponent } from './component/principal/principal.component';
+import { CarouselComponent } from './component/carousel/carousel.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavBarComponent, PrincipalComponent ],
+  declarations: [ AppComponent, HelloComponent, NavBarComponent, PrincipalComponent, CarouselComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
