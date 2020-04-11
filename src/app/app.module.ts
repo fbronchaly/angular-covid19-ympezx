c24e56107628fecda63d36aa90a126c6a331745f
fbronchaly <fbronchaly@gmail.com>
2020-04-11 19:55:30 +0200

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,11 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavBarComponent } from './component/shared/nav-bar/nav-bar.component';
 import { PrincipalComponent } from './component/principal/principal.component';
-import { CarouselComponent } from './component/carousel/carousel.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavBarComponent, PrincipalComponent, CarouselComponent ],
+  declarations: [ AppComponent, HelloComponent, NavBarComponent, PrincipalComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
