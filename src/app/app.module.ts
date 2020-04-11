ad06ccb1da1ecd22a9b14a59cfef7ccbd4aac2b4
fbronchaly <fbronchaly@gmail.com>
2020-04-11 18:04:44 +0200

    3 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { NavBarComponent } from './component/shared/nav-bar/nav-bar.component';
+import { PrincipalComponent } from './component/principal/principal.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavBarComponent ],
+  declarations: [ AppComponent, HelloComponent, NavBarComponent, PrincipalComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
