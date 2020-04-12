ecec6f4c03c160f8354275190bec12c4ecf06103
fbronchaly <fbronchaly@gmail.com>
2020-04-12 07:01:00 +0200

    2 files modified, 9 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { NavBarComponent } from './component/shared/nav-bar/nav-bar.component';
 import { PrincipalComponent } from './component/principal/principal.component';
+import { CabeceraComponent } from './component/cabecera/cabecera.component';
+import { BodyComponent } from './component/body/body.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, NavBarComponent, PrincipalComponent ],
+  declarations: [ AppComponent, HelloComponent, PrincipalComponent, CabeceraComponent, BodyComponent, FooterComponent, NavbarComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
