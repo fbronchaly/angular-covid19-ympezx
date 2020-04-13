2e3b422dfa879cac5188baf21a740b7eeda7de2f
fbronchaly <fbronchaly@gmail.com>
2020-04-13 05:45:33 +0200

    4 files modified, 3 added, 8 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,19 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { PrincipalComponent } from './component/principal/principal.component';
 import { CabeceraComponent } from './component/cabecera/cabecera.component';
 import { BodyComponent } from './component/body/body.component';
 import { FooterComponent } from './component/footer/footer.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { Panel1Component } from './component/panel1/panel1.component';
 import { Panel2Component } from './component/panel2/panel2.component';
+import { ConsentimientoComponent } from './component/consentimiento/consentimiento.component';
 
 
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule,APP_ROUTING,RouterModule ],
-  declarations: [ AppComponent, HelloComponent, PrincipalComponent, CabeceraComponent, BodyComponent, FooterComponent, NavbarComponent, Panel1Component, Panel2Component],
+  declarations: [ AppComponent, HelloComponent, CabeceraComponent, BodyComponent, FooterComponent, NavbarComponent, Panel1Component, Panel2Component, ConsentimientoComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { Panel1Component } from './component/panel1/panel1.component';
 import { Panel2Component } from './component/panel2/panel2.component';
+import { ConsentimientoComponent } from './component/consentimiento/consentimiento.component';
 
 const APP_ROUTES: Routes = [
   { path: 'investigadoras', component: Panel1Component },
   { path: 'instrucciones', component: Panel2Component },
+  { path: 'consentimiento', component:ConsentimientoComponent },
  { path: '**', pathMatch: 'full', redirectTo: 'investigadoras' }
 ];
 
