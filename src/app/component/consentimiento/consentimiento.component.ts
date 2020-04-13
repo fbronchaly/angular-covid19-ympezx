37768669c0bae792e5b1e1aa0fcf263a9ac240ae
fbronchaly <fbronchaly@gmail.com>
2020-04-13 09:13:15 +0200

    2 files modified, 1 added

--- src/app/component/consentimiento/consentimiento.component.ts
+++ src/app/component/consentimiento/consentimiento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-consentimiento',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConsentimientoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public auth:AuthService
+  ) { }
 
   ngOnInit() {
   }
